13b56703633f46714bd795f74c68494ad62f31f2
EitharAlotoom95 <eitharalatoom@gmail.com>
2020-11-09 02:38:49 +0200

    render data from continent

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { onMount } from "svelte";
+	import Continent from "./Continent.svelte";
+	import Continents from "./Continents.svelte";
+	let continents;
+	let continent;
+  </script>
+  
+  <h1>The Seven Continents Svelte App</h1>
+  <main>
+	  <Continents {continents} />
+  </main>
+
+
+  <svelte:head>
+  <title>Svelte Continent App</title>
+</svelte:head>
+
+
+<style>
+  main {
+    background-color: teal;
+    font-size: 15px;
+  }
+  h1 {
+    font-size: 25px;
+  }
+</style>
